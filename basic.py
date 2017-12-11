0309f26af502ddd6e50b0842af5c847faea35b9a
DOUBLE U <realnieopasnie@gmail.com>
2017-12-11 12:44:11 +0300

    Update basic.py

--- basic.py
+++ basic.py
@@ -5,11 +5,11 @@ else:
     password = input("Nice, " + reg + ", now enter a password: ")
 if reg != '':
     login = input("Awesome! Now log in the system using your username: ")
-if login == reg:
-    loginPass = input("Nice work, now enter your password: ")
-    if loginPass == password:
-        print("Yup got it. Your username is " + reg + " and your password is " + password)
+    if login == reg:
+        loginPass = input("Nice work, now enter your password: ")
+        if loginPass == password:
+            print("Yup got it. Your username is " + reg + " and your password is " + password)
+        else:
+            print("Nope, ending program...")
     else:
         print("Nope, ending program...")
-else:
-    print("Nope, ending program...")
