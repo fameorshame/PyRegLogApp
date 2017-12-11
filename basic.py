aeca8cb6fb0c9b7a7a7220509e1c1f267002375a
DOUBLE U <realnieopasnie@gmail.com>
2017-12-11 12:13:28 +0300

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,15 @@
+reg = input(str("Enter the username you would like: "))
+if reg == '':
+    print("Incorrect, enter a valid username")
+else:
+    password = input("Nice, " + reg + ", now enter a password: ")
+if reg != '':
+    login = input("Awesome! Now log in the system using your username: ")
+if login == reg:
+    loginPass = input("Nice work, now enter your password: ")
+    if loginPass == password:
+        print("Yup got it. Your username is " + reg + " and your password is " + password)
+    else:
+        print("Nope, ending program...")
+else:
+    print("Nope, ending program...")
